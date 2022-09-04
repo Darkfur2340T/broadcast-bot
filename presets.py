710337e83c5bf5076c6090fe6cefb2c1026bb1db
Darkfur2340T <97725597+Darkfur2340T@users.noreply.github.com>
2022-09-04 17:35:53 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,9 +1,9 @@
 class Presets(object):
     WELCOME_MESSAGE = """
 <b>Hello.. </b>{}
-<code>I'm an official telecaster bot
+<code>I'm an News bot
 maintained by</code> <b>{}</b>
-<code>Keep me unblocked to receive ma official updates.</code>
+<code>Keep me unblocked to receive all news updates.</code>
     """
     USERS_LIST = "<b>Total:</b>\n\nSubscribers - {}\nBlocked / Deleted - {}"
     WAIT_MSG = "<b>Please Wait...</b>"
